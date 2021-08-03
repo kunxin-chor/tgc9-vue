e81f917cc04bb6e2135acf73fc38dbf864570376
kunxin-chor <chorkunxin@yahoo.com>
2021-08-03 08:13:04 +0000

    Add store

--- 14-share-data-across-routes/src/store/index.js
+++ 14-share-data-across-routes/src/store/index.js
@@ -5,6 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    customers:[
+      {
+        "id": 1,
+        "name": "Kelvin Ho",
+        "email": "kelvin@gemail.com"
+      },
+      {
+        "id": 2,
+        "name": "Tan Ah Mew",
+        "email": "tanahmew@gemail.com"
+      },
+      {
+        "id": 3,
+        "name": "Susan Tan",
+        "email": "susan@gemail.com"
+      }
+    ]
   },
   mutations: {
   },
