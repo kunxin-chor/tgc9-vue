dede0ea11e10e53d33317ab6be27726e3e5aa69f
kunxin-chor <chorkunxin@yahoo.com>
2021-08-03 07:52:58 +0000

    Added views

---
+++ 14-share-data-across-routes/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/add">Add</router-link> | 
+      <router-link to="/edit">Edit</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ 14-share-data-across-routes/src/views/Add.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Add Customer</h1>
+</template>
+
+<script>
+export default {
+    name:'Add'
+}
+</script>
